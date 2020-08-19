e9758be79af285b3c8645d852504fe34161a7355
TsonT <32753215+TsonT@users.noreply.github.com>
2020-08-18 23:58:44 -0400

    Add files via upload

---
+++ 203_Fibonacci/203_Fibonacci/203_Fibonacci/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace _203_Fibonacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(77, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(129, 22);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "=";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(234, 12);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(129, 22);
+            this.txtAnswer.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 83);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(107, 31);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(169, 83);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(107, 31);
+            this.btnPrevious.TabIndex = 4;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(326, 83);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(107, 31);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 126);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ 203_Fibonacci/203_Fibonacci/203_Fibonacci/Form1.cs
@@ -0,0 +1,110 @@
+﻿//Nicholas Tran
+//10/7/18
+//Fourth
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _203_Fibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //converts text into integer
+            int Input = Convert.ToInt16(txtInput.Text);
+
+            txtInput.Text = txtInput.Text + Term.Correct(Input);
+
+            txtAnswer.Text = Convert.ToString(Fibonacci.Calculate(Input));
+        }
+
+        //Previous button click event
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            //Determines the proper suffix of the number
+            string strText = txtInput.Text;
+
+            strText = strText.Remove(strText.Length - 1);
+            strText = strText.Remove(strText.Length - 1);
+
+            int Input = Convert.ToInt16(strText);
+
+            Input = Input - 1;
+
+            txtInput.Text = Input + Term.Correct(Input);
+
+            txtAnswer.Text = Convert.ToString(Fibonacci.Calculate(Input));
+        }
+
+        //Next button click
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            string strText = txtInput.Text;
+
+            strText = strText.Remove(strText.Length - 1);
+            strText = strText.Remove(strText.Length - 1);
+
+            int Input = Convert.ToInt16(strText);
+
+            Input = Input + 1;
+
+            txtInput.Text = Input + Term.Correct(Input);
+
+            txtAnswer.Text = Convert.ToString(Fibonacci.Calculate(Input));
+        }
+
+        //Fibancci class
+        static class Fibonacci
+        {
+            public static int Calculate(int num)
+            {
+                if (num <= 2)
+                {
+                    return 1;
+                }
+
+                return Calculate(num - 1) + Calculate(num - 2);
+            }
+        }
+
+        //Finds the correct suffix
+        static class Term
+        {
+            public static string Correct(int Input)
+            {
+                int LastDigit = Input % 10;
+
+                if (LastDigit == 1)
+                {
+                    return "st";
+                }
+
+                else if (LastDigit == 2)
+                {
+                    return "nd";
+                }
+
+                else if (LastDigit == 3)
+                {
+                    return "rd";
+                }
+
+                else
+                {
+                  return "th";
+                }
+            }
+        }
+    }
+}
